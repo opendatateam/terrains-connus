a6a555f170701e2d57e74747a5dce2650ef9920a
Adrien Carpentier <me@adriencarpentier.com>
2024-12-02 18:45:27 +0100

    Squashed commit of the following:
    commit 259aea3933e9a929065a4cef81935b2547576bd0
    Author: Adrien Carpentier <me@adriencarpentier.com>
    Date:   Mon Dec 2 18:45:13 2024 +0100
    
        fix: organise sidebar

---
+++ frontend/src/components/sidebar/TileTwo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tile">
+    <h2 class="font-bold mb-2">{{ natureCultureFullName }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { NatureCulture } from "@/types/enums/NatureCulture";
+
+export default defineComponent({
+  name: "TileTwo",
+  props: {
+    selectedNatureCulture: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    natureCultureFullName(): string {
+      return this.selectedNatureCulture ? NatureCulture[this.selectedNatureCulture as keyof typeof NatureCulture] : "";
+    }
+  }
+});
+</script>
