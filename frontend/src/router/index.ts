6edda3e23617ffb299bd4a05318fbc62eeada90d
Adrien Carpentier <me@adriencarpentier.com>
2024-12-03 15:29:18 +0100

    fix: attempt to fix 404 issue when reloading

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import MapView from "../views/MapView.vue";
 import QuestionsView from "../views/QuestionsView.vue";
 import TableauView from "../views/TableauView.vue";
 
 const router = createRouter({
-	history: createWebHistory("/terrains-connus/"),
+	history: createWebHashHistory("/terrains-connus/"),
 	routes: [
 		{
 			path: "/",
